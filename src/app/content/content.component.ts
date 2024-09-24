591d1b13d7bc3e1d62ebbe5815882da00eecc895
samuelpd22 <samuel050101@hotmail.com>
2024-09-24 20:51:42 -0300

    feat: mensagem de erro

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -8,6 +8,8 @@ import { AxiosService } from '../axios.service';
 })
 export class ContentComponent {
 	componentToShow: string = "welcome";
+	errorMessage: string | null = null; // Variável para mensagem de erro
+
 
 	constructor(private axiosService: AxiosService) { }
 
@@ -22,18 +24,21 @@ export class ContentComponent {
 		    {
 		        login: input.login,
 		        password: input.password
-		    }).then(
-		    response => {
-		        this.axiosService.setAuthToken(response.data.token);
-		        this.componentToShow = "messages";
-		    }).catch(
-		    error => {
-		        this.axiosService.setAuthToken(null);
-		        this.componentToShow = "welcome";
-		    }
-		);
-
-	}
+		    }).then(response => {
+				this.axiosService.setAuthToken(response.data.token);
+				this.componentToShow = "messages";
+				this.errorMessage = null; // Limpa mensagem de erro em caso de sucesso
+			}).catch(error => {
+				this.axiosService.setAuthToken(null);
+				this.componentToShow = "welcome";
+				this.errorMessage = error.response?.data?.message || "Ocorreu um erro ao fazer login."; // Exibe a mensagem do backend ou uma mensagem padrão
+				
+				// Limpa a mensagem após 5 segundos
+				setTimeout(() => {
+					this.errorMessage = null;
+				}, 3000); // 5000 milissegundos = 5 segundos
+			});
+		}
 
 	onRegister(input: any): void {
 		this.axiosService.request(
@@ -44,16 +49,19 @@ export class ContentComponent {
 		        lastName: input.lastName,
 		        login: input.login,
 		        password: input.password
-		    }).then(
-		    response => {
-		        this.axiosService.setAuthToken(response.data.token);
-		        this.componentToShow = "messages";
-		    }).catch(
-		    error => {
-		        this.axiosService.setAuthToken(null);
-		        this.componentToShow = "welcome";
-		    }
-		);
+		    }).then(response => {
+				this.axiosService.setAuthToken(response.data.token);
+				this.componentToShow = "messages";
+				this.errorMessage = null; // Limpa mensagem de erro em caso de sucesso
+			}).catch(error => {
+				this.axiosService.setAuthToken(null);
+				this.componentToShow = "welcome";
+				this.errorMessage = error.response?.data?.message || "Ocorreu um erro ao registrar."; // Exibe a mensagem do backend ou uma mensagem padrão
+				
+				// Limpa a mensagem após 5 segundos
+				setTimeout(() => {
+					this.errorMessage = null;
+				}, 3000); // 5000 milissegundos = 5 segundos
+			});
+		}
 	}
-
-}
